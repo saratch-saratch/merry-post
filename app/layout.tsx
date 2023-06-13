953c4707d490d2cfc757011127ec4aa0a03d0d75
Saratch Tanapongwonglert <saratch.saratch@gmail.com>
2023-06-13 23:04:06 +0100

    Update: Index layout to enable dual pages display

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Home from "./Home";
 
 export const metadata = {
   title: "Merry Post",
@@ -11,13 +12,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="h-full">
-      <body className="h-full bg-neutral-800">
-        <main className=" flex h-full flex-col ">
-          <div>
-            <section>Home</section>
+    <html lang="en">
+      <body>
+        <main className="flex bg-neutral-800">
+          <Home />
+          <div className="h-screen w-full  flex-col">
+            <header className="sticky top-0 flex h-12 items-center border-b border-b-neutral-700 bg-neutral-900 px-4 py-2"></header>
+            <section className="mt-12">{children}</section>
           </div>
-          <div>{children}</div>
         </main>
       </body>
     </html>
