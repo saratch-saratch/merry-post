d298e9b5f21488cab30c5c83f0666646b035006a
Saratch Tanapongwonglert <saratch.saratch@gmail.com>
2023-06-19 13:34:23 +0100

    Add: Message bar for post page and POST API

---
+++ app/posts/[postId]/MessageBar.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { FormEvent, useState } from "react";
+import { mutate } from "./Comments";
+
+export default function MessageBar({ postId }: { postId: string }) {
+  const router = useRouter();
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    await fetch(
+      process.env.NEXT_PUBLIC_API_URL + "/posts/" + postId + "/comments",
+      {
+        method: "POST",
+        body: JSON.stringify({ message: message }),
+      }
+    );
+    setMessage("");
+    mutate();
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="sticky bottom-0 flex h-12 rounded-b-lg  bg-neutral-900 px-4 py-2"
+    >
+      <input
+        type="text"
+        name="message"
+        placeholder="Send a message in this post"
+        className="w-full rounded-md bg-neutral-800 px-4 outline-none"
+        value={message}
+        onChange={(event) => setMessage(event.target.value)}
+      ></input>
+    </form>
+  );
+}
